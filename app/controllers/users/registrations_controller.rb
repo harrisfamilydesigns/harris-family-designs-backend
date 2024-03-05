6994f21e5cd05ea623806d4246482c573e35953b
Max Harris <mharris7190@gmail.com>
2024-03-05 09:07:58 -0500

    sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,4 +10,32 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+
+  def update
+    # if unpermitted parameters are sent, return 422 Unprocessable Entity
+    return render json: { errors: ['Something went wrong'] }, status: :unprocessable_entity if unpermitted_params?
+
+    super do |resource|
+      if resource.errors.empty?
+        render json: resource, status: :ok and return
+      else
+        render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity and return
+      end
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  # Do not allow to change admin attribute
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
+
+  def unpermitted_params?
+    params.require(:user).keys.map(&:to_sym).any? { |param| !account_update_params.keys.map(&:to_sym).include?(param) }
+  end
 end
