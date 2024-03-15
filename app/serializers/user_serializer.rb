cd2675e510c8827b12ee6f0be89db12af2aaff7a
Max Harris <mharris7190@gmail.com>
2024-03-15 15:20:08 -0400

    fix first time confirmation

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,6 +10,7 @@ class UserSerializer < BaseSerializer
       email: @user.email,
       first_name: @user.first_name,
       last_name: @user.last_name,
+      confirmed: @user.confirmed?,
       unconfirmed_email: @user.unconfirmed_email,
     }
     if @user.admin?
