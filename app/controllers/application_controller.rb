a4817b80fda456ab2441a228dd1f48dd7aab0d36
Max Harris <mharris7190@gmail.com>
2024-04-09 16:24:50 -0400

    fix serving secondhandfix from rails server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,43 +9,47 @@ class ApplicationController < ActionController::API
   before_action :snake_case_params
 
   def serve_from_gcs
-    path = request.path
-    path = path[1..-1] if path.starts_with?("/")
-    path = path.chomp("/")
+    # Normalize the path
+    path = request.path.delete_prefix('/').chomp('/')
 
     if request.subdomain == 'secondhand'
       bucket = storage_client.bucket('secondhandfix') # reactnative web build
-      file = path ? bucket.file(path) : bucket.file("index.html")
+      file = find_file_in_bucket(bucket, path)
+      file ||= bucket.file("index.html") # When the path is just a frontend route
     else
       bucket = storage_client.bucket('hfd-fe') # vite build
       is_asset_path = path.starts_with?("assets/") || path.match?(extension)
       file = is_asset_path ? bucket.file(path) : bucket.file("index.html")
+      file ||= bucket.file("index.html")
     end
 
-    if file.nil?
-      file = bucket.file("index.html")
+    if file
+      content = file.download
+      content.rewind
+      send_data content.read, type: file.content_type, disposition: "inline"
+    else
+      record_not_found
     end
-    content = file.download
-    content.rewind
-    send_data content.read, type: file.content_type, disposition: "inline"
+  rescue Google::Cloud::NotFoundError => e
+    record_not_found
   rescue StandardError => e
     render json: { error: e.message }, status: :internal_server_error
   end
 
   def record_not_found
-    render json: { error: 'Record not found' }, status: :not_found
+    render json: { error: 'Not found' }, status: :not_found
   end
 
   def record_invalid
-    render json: { error: 'Record invalid' }, status: :unprocessable_entity
+    render json: { error: 'Invalid' }, status: :unprocessable_entity
   end
 
   def record_not_saved
-    render json: { error: 'Record not saved' }, status: :unprocessable_entity
+    render json: { error: 'Not saved' }, status: :unprocessable_entity
   end
 
   def record_not_destroyed
-    render json: { error: 'Record not destroyed' }, status: :unprocessable_entity
+    render json: { error: 'Not destroyed' }, status: :unprocessable_entity
   end
 
   def unpermitted_parameters(exception)
@@ -73,5 +77,23 @@ class ApplicationController < ActionController::API
 
   def snake_case_params
     params.deep_transform_keys!(&:underscore)
+  end
+
+  def find_file_in_bucket(bucket, path)
+    # Check if the requested path directly maps to a file
+    # When it's a direct_file, the kind is "storage#object",
+    # when it's a directory it's "storage#objects"
+    file = bucket.file(path)
+    is_direct_file = file.present? && file.kind == "storage#object"
+    return file if is_direct_file
+
+    # For directory requests, try serving "index.html" within the directory
+    if path.blank? || path.end_with?('/')
+      directory_index = "#{path}index.html"
+    else
+      directory_index = "#{path}/index.html"
+    end
+
+    bucket.file(directory_index)
   end
 end
