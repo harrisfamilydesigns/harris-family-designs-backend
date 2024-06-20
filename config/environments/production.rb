c2a2b68d9cc9ab9cf910ca1454d594d7ef29eb9a
Max Harris <mharris7190@gmail.com>
2024-06-20 15:32:16 -0400

    only allow requests to api.harrisfamilydesigns.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,10 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "api.harrisfamilydesigns.com",
-    /harris-family-designs-backend-*\.a\.run\.app/ # Allow requests from Cloud Run services like `example-uc.a.run.app`
-  ]
+  config.hosts = ["api.harrisfamilydesigns.com"]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
